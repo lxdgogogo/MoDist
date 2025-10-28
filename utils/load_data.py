30e769274b0bb0ec3b51baf68e08acc01b58308b
lxdgogogo <1636614463@qq.com>
2025-10-28 16:17:06 +0800

    update

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,7 +3,7 @@ import torch
 
 
 def load_data(dataset, device):
-    file_path = f"./dataset/{dataset}"
+    file_path = f"./dataset/{dataset}.dgl"
     graph = dgl.load_graphs(file_path)[0][0].to(device)
     graph = dgl.remove_self_loop(graph)
     graph = dgl.add_self_loop(graph)
